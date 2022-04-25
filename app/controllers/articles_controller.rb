0c58e7b6e6fbc37185c2e9c21a59e6de3a63d0fc
Braden Kinard <braden.kinard@mx.com>
2022-04-25 15:52:14 -0400

    add article edit and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,9 +15,23 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect to @article
+      redirect_to @article
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
